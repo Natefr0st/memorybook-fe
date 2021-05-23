019c72ba5a4ea28c3eac26ab563b7ef22f75b206
Kevin <kevin_zino@yahoo.com>
2021-05-23 14:46:43 +0300

    Implement form functionallity

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -1,10 +1,33 @@
 import * as api from '../api';
 
+/**
+ * Get Posts Action Creator
+ * @returns {void}
+ */
 export const getPosts = () => async (dispatch) => {
 	try {
 		const { data } = await api.fetchPosts();
+
 		dispatch({
 			type: 'FETCH_ALL',
+			payload: data,
+		});
+	} catch (error) {
+		console.log(error.message);
+	}
+};
+
+/**
+ * Create Post Action Creator
+ * @param {object} post Body
+ * @returns {void}
+ */
+export const createPost = (post) => async (dispatch) => {
+	try {
+		const { data } = await api.createPost(post);
+
+		dispatch({
+			type: 'CREATE',
 			payload: data,
 		});
 	} catch (error) {
