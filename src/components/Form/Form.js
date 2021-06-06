e54e274edcca1e0b2c991fc6844e73fbf16f4155
Kevin <kevin_zino@yahoo.com>
2021-06-06 22:28:08 +0300

    Implement Update Functionallity

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import FileBase from 'react-file-base64';
 import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import useStyles from './styles';
-import { createPost } from '../../actions/posts';
+import { createPost, updatePost } from '../../actions/posts';
 
-const Form = () => {
+const Form = ({ currentId, setCurrentId }) => {
 	const [postData, setPostData] = useState({
 		creator: '',
 		title: '',
@@ -14,16 +15,40 @@ const Form = () => {
 		tags: '',
 		selectedFile: '',
 	});
+	const post = useSelector((state) =>
+		currentId ? state.posts.find((post) => post._id === currentId) : null
+	);
 	const classes = useStyles();
 	const dispatch = useDispatch();
+
+	useEffect(() => {
+		if (post) {
+			setPostData(post);
+		}
+	}, [post]);
 
 	const handleSubmit = (event) => {
 		event.preventDefault();
 
-		dispatch(createPost(postData));
+		if (currentId) {
+			dispatch(updatePost(currentId, postData));
+		} else {
+			dispatch(createPost(postData));
+		}
+
+		clear();
 	};
 
-	const clear = () => {};
+	const clear = () => {
+		setCurrentId(null);
+		setPostData({
+			creator: '',
+			title: '',
+			message: '',
+			tags: '',
+			selectedFile: '',
+		});
+	};
 
 	return (
 		<Paper className={classes.paper}>
@@ -33,14 +58,18 @@ const Form = () => {
 				className={`${classes.root} ${classes.form}`}
 				onSubmit={handleSubmit}
 			>
-				<Typography variant="h6">Creating a Memory</Typography>
+				<Typography variant="h6">
+					{currentId ? 'Editing' : 'Creating'} a Memory
+				</Typography>
 				<TextField
 					name="creator"
 					variant="outlined"
 					label="Creator"
 					fullWidth
 					value={postData.creator}
-					onChange={(event) => setPostData({  ...postData, creator: event.target.value  })}
+					onChange={(event) =>
+						setPostData({ ...postData, creator: event.target.value })
+					}
 				/>
 				<TextField
 					name="title"
@@ -48,7 +77,9 @@ const Form = () => {
 					label="Title"
 					fullWidth
 					value={postData.title}
-					onChange={(event) => setPostData({  ...postData, title: event.target.value  })}
+					onChange={(event) =>
+						setPostData({ ...postData, title: event.target.value })
+					}
 				/>
 				<TextField
 					name="message"
@@ -56,7 +87,9 @@ const Form = () => {
 					label="Message"
 					fullWidth
 					value={postData.message}
-					onChange={(event) => setPostData({  ...postData, message: event.target.value  })}
+					onChange={(event) =>
+						setPostData({ ...postData, message: event.target.value })
+					}
 				/>
 				<TextField
 					name="tags"
@@ -64,30 +97,34 @@ const Form = () => {
 					label="Tags"
 					fullWidth
 					value={postData.tags}
-					onChange={(event) => setPostData({  ...postData, tags: event.target.value  })}
+					onChange={(event) =>
+						setPostData({ ...postData, tags: event.target.value })
+					}
 				/>
 				<div className={classes.fileInput}>
-					<FileBase 
-						type="file" 
+					<FileBase
+						type="file"
 						multiple={false}
-						onDone={({base64}) => setPostData({...postData, selectedFile: base64})} 
+						onDone={({ base64 }) =>
+							setPostData({ ...postData, selectedFile: base64 })
+						}
 					/>
 				</div>
 				<Button
-					className={classes.buttonSubmit} 
-					variant="contained" 
-					color="primary" 
-					size="large" 
-					type="submit" 
+					className={classes.buttonSubmit}
+					variant="contained"
+					color="primary"
+					size="large"
+					type="submit"
 					fullWidth
 				>
 					Submit
 				</Button>
 				<Button
-					variant="contained" 
-					color="secondary" 
-					size="small" 
-					onClick={clear} 
+					variant="contained"
+					color="secondary"
+					size="small"
+					onClick={clear}
 					fullWidth
 				>
 					Clear
